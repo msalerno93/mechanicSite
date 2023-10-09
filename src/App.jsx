b5b890e2e00996aad1b31f04c7ef4ba786f76ce9
Michael Salerno <msalerno1993@gmail.com>
2023-10-09 14:12:53 -0400

    working on links in navbar and pages within application

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import NavBar from "./components/NavBar";
+import AboutUs from "./pages/AboutUs";
+import { Route, Routes } from "react-router";
 
 
 function App() {
 
   return (
     <>
-    <NavBar />
+    <NavBar>
+      <Routes>
+      <Route path="/aboutus" element={<AboutUs />}/>
+      </Routes>
+    </NavBar>
     </>
   )
 }
